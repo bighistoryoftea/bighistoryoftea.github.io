a54b1a80ca959c49d18991c20811eea774b9477e
isaacaddis <isaacaddis21@gmail.com>
2017-04-18 13:18:16 -0700

    Start

--- tea.js
+++ tea.js
@@ -6,6 +6,7 @@ document.onkeydown = function(e){
     switch(e.keyCode){
         case 37:
             // Left
+            alert("Working");
         case 38:
             //Up
         case 39:
@@ -15,3 +16,11 @@ document.onkeydown = function(e){
     }
 }
 });
+/*
+  Guide:
+  Paragraph 0 - Introduction and Guide
+  Paragraph 1 -
+*/
+var textParagraphs = [
+  [0,"<p>Hello! Press the <strong>right key</strong> to continue onto the first section (Origins of Tea).</p>"],
+];
